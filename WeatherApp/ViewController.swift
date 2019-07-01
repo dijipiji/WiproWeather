b6df343a0388ca4f40224cecfcda091cc65e8290
Jamie Lemon <jamie.lemon@dijipiji.com>
2019-07-01 09:51:11 +0100

    initial commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Jamie Lemon on 01/07/2019.
+//  Copyright © 2019 dijipiji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
